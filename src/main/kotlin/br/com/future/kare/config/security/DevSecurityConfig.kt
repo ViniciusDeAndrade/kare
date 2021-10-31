97ab64300701e9f921125a64c8b30a5589d64cc4
Vinicius de Andrade <viniciusclo@gmail.com>
2021-10-31 16:15:53 -0300

    swagger

--- src/main/kotlin/br/com/future/kare/config/security/DevSecurityConfig.kt
+++ src/main/kotlin/br/com/future/kare/config/security/DevSecurityConfig.kt
@@ -4,6 +4,7 @@ package br.com.future.kare.config.security
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
@@ -15,5 +16,12 @@ class DevSecurityConfig : WebSecurityConfigurerAdapter(){
     override fun configure(http: HttpSecurity) {
         http.authorizeRequests().antMatchers("/**").permitAll()
                 .and().csrf().disable();
+    }
+
+    /**
+     *  static resources configuration (js, css ...)
+     */
+    override fun configure(web: WebSecurity) {
+        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**")
     }
 }
